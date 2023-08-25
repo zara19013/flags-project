7b17d14d89e72daa270f4377aaeee2ce09c13ac9
zara19013 <bsce19013@itu.edu.pk>
2023-08-25 19:13:15 +0500

    home page done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { createContext, useEffect, useState } from 'react';
 import './App.css';
 import Filter from './components/Home page/filter';
-// import All from './components/Home page/feild_container';
 import { Card } from './components/Home page/card';
 import Header from './components/Header';
 import axios from 'axios';
@@ -9,13 +8,7 @@ export const RootContext = createContext();
 function App() {
   const [countriesData, setCountriesData] = useState([]);
   const [filteredCountries, setFilteredCountries] = useState([]);
-  
-  // useEffect(() => {
-  //   axios.get("https://restcountries.com/v3.1/all")
-  //     .then(response => setCountries(response.data))
-      
-  //     .catch(error => console.error(error))
-  // }, []);
+
   useEffect(() => {
     axios.get("https://restcountries.com/v3.1/all")
       .then(response => {
@@ -30,33 +23,7 @@ function App() {
 
 
   return (
-    // <div className="App">
-    //   <Header />
-    //   {/* <All/> */}
-    //   {/* <Filter/> */}
-    //   <div className="contents"> 
-    //   <div>
-    //   <div className="feild-header">
-    //   </div>
-    //     <div className="card-container">
-    //       {/* <button className="backBtn">Back</button> */}
-    //       {countries.map(country => (
-    //         <div className="card-container" key={country.name.common}>
-    //           <div className="flag">
-    //             <Card
-    //               imageUrl={country.flags.png}
-    //               countryName={country.name.common}
-    //               population={country.population}
-    //               region={country.region}
-    //               capital={country.capital}
-    //             />
-    //           </div>
-    //         </div>
-    //       ))}
-    //     </div>
-    //   </div>
-    // </div>
-    // </div>
+
     <div className="App">
     <Header />
     <Filter countriesData={countriesData} onFilterChange={handleFilterChange} />
@@ -85,81 +52,3 @@ function App() {
 }
 
 export default App;
-
-
-
-//  import React, {createContext, useState, useEffect }  from 'react';
-// import './App.css'
-// import axios from 'axios';
-// import Header from './components/Header';
-
-// export const RootContext = createContext();
-// export const PictureCard = ({ imageUrl, countryName, population, region, capital }) => {
-//   return (
-//     <div className='card-container'>
-//       <img src={imageUrl} alt="countryName" className="picture-image" />
-//       <div className="description">
-//        <h2 className="countryName">{countryName}</h2> 
-//       <div className="population">
-//         <span className="title">Population: </span>
-//         {population}
-//       </div>
-//       <div className="region">
-//         <span className="title">Region: </span>
-//         {region}
-//       </div>
-//       <div className="capitanpl">
-//         <span className="title">Capital: </span>
-//         {capital}
-//       </div>
-//       </div>
-      
-//     </div>   
-//   );
-// };
-
-// function App() {
-//  const [countries, setcountries]= useState([]);
-//   useEffect(()=>{
-//     axios.get("https://restcountries.com/v3.1/all")
-//     .then(response => setcountries(response.data))
-//     .catch(error => console.error(error))
-//   })
-//   return (
-// <div className="App">
-//   <Header/>
-      
-//       <div className="card-container">
-//       {countries.map(country => (
-//         <div className="card-container" key={country.name.common}>
-//           <div className="flag">
-//             <PictureCard
-//               imageUrl={country.flags.png}
-//               countryName={country.name.common}
-//               population={country.population}
-//               region={country.region}
-//               capital={country.capital}
-//             />
-//           </div>
-//         </div>
-//       ))}
-     
-        
-//       </div>
-//       {/* <div className="card-container">
-//         <div className="flag">
-//           <PictureCard 
-//           imageUrl={imageUrl}
-//           countryName="Pakistan"
-//           population="23456789"
-//           region="Asia"
-//           capital="Islambad"
-//           />
-//         </div>
-//       </div> */}
-//       </div>
-
-//   );
-// }
-
-// export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -21,15 +21,6 @@ function Header() {
       <a href="/App.js" className="headerTitle">
          <p> Where in the world?</p> 
         </a>
-
-        {/* <p className="headerTitle" >
-          Where in the world?
-        </p> */}
-        {/* <div className="modeToggle" onClick={toggleColorMode}>
-          <i className="fas fa-moon modeIcon"></i>
-          <span className="modeText">Dark Mode</span>
-        </div>
-      </div> */}
         </div>
     
     </header>
@@ -37,40 +28,3 @@ function Header() {
 }
 
 export default Header;
-
-// import React, { useContext} from 'react';
-// import './Header.css';
-// // import { RootContext } from '../App';
-
-// export default function Header() {
-//     return (
-//         <div className="App-header">
-//                 <p>Where in the world?</p>
-//         </div>
-//     )
-// }
-
-// function Header() {
-//     const [, , , , , setIsAll, colorMode, setColorMode] = useContext(RootContext);
-  
-//     function goHome() {
-//       setIsAll(true);
-//     }
-//     function changeMode() {
-//       (colorMode === "light") ? setColorMode("dark") : setColorMode("light")
-//     }
-  
-//     return (
-//       <header className="header">
-//         <div className="container">
-//           <p className="headerTitle" onClick={goHome}>Where in the world?</p>
-//           <p onClick={changeMode}>
-//             <i className="fas fa-moon moonIcon"></i>
-//             Dark Mord
-//           </p>
-//         </div>
-//       </header>
-//     );
-//   }
-  
-//   export default Header;
