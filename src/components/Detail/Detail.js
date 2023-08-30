e9b85d239959f4c476b49a23a9712793cff8e10b
zara19013 <bsce19013@itu.edu.pk>
2023-08-30 15:49:01 +0500

    border countries done

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,69 +1,3 @@
-// layout dessign
-// import React, { useEffect, useState } from 'react';
-// import './detail.css';
-// import App from '../../App';
-
-// function DetailsPage() {
-//   const countryData = {
-//     name: 'Algeria',
-//     nativeName: 'الجزائر',
-//     population: 43000000,
-//     region: 'Africa',
-//     subregion: 'Northern Africa',
-//     capital: 'Algiers',
-//     topLevelDomain: '.dz',
-//     currency: 'Algerian Dinar (DZD)',
-//     languages: ['Arabic', 'Berber'],
-//     borderCountries: ['Tunisia', 'Libya', 'Niger', 'Mali', 'Mauritania', 'Western Sahara'],
-//   };
-
-// return (
-
-//       <div  className="contents">
-//       <div className="container">
-//         <div className="backBtn">
-//           <span className="backArrow">←</span> Back
-//         </div>
-
-//         <div className="wrapDetail">
-//           <div className="leftFlag">
-//             <img src="https://media.istockphoto.com/id/652750800/vector/pakistan.jpg?s=612x612&w=0&k=20&c=x14F0XneN74dfVp2qL_vfT8JCZaHRB8ZKUIsrf0lqGY=" alt="Algeria Flag" className="leftFlag" />
-//           </div>
-//           <div className="rightDetail">
-//             <h2 className="countryName">{countryData.name}</h2>
-//             <div className="wrapInsideDetail">
-//               <div className="leftInsideDetail">
-//               <p><strong>Native Name:</strong> {countryData.nativeName}</p>
-//                 <p><strong>Population:</strong> {countryData.population.toLocaleString()}</p>
-//                 <p><strong>Region:</strong> {countryData.region}</p>
-//                 <p><strong>Sub Region:</strong> {countryData.subregion}</p>
-//                <p><strong>Capital:</strong> {countryData.capital}</p>
-//                </div>
-//                <div className="rightInsideDetail">
-//                <p><strong>Top Level Domain:</strong> {countryData.topLevelDomain}</p>
-//               <p><strong>Currency:</strong> {countryData.currency}</p>
-//                 <p><strong>Languages:</strong> {countryData.languages.join(', ')}</p>
-//               </div>
-//             </div>
-//             <div className="borderCountries">
-//               <div className="title">Border Countries:</div>
-//               <div className="wrapBorderCountries">
-//                 {countryData.borderCountries.map((borderCountry, index) => (
-//                   <button className="borderCountry" key={index}>
-//                     {borderCountry}
-//                   </button>
-//                 ))}
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default DetailsPage;
-//--------------------------------------------------------------------------- layout design
 import React, { useEffect, useState } from "react";
 import "./detail.css";
 import { Link } from "react-router-dom";
@@ -82,7 +16,6 @@ function DetailsPage() {
   useEffect(() => {
     axios
       .get(`https://restcountries.com/v3.1/alpha/${countryCode}`)
-      // .get(`https://restcountries.com/v3.1/alpha/pk`)
       .then((response) => {
         const fetchedCountryData = response.data[0];
         setCountryData(fetchedCountryData);
@@ -183,12 +116,12 @@ function DetailsPage() {
               <div className="title">Border Countries:</div>
               <div className="wrapBorderCountries">
                 {borderCountryNames.map((borderCountryName, index) => (
-             <Link
-             to={`/border-details?code=${countryData.borders[index]}`}
-             className="borderCountry"
-           >
-             {borderCountryName}
-           </Link>
+                  <Link
+                    to={`/border-details?code=${countryData.borders[index]}`}
+                    className="borderCountry"
+                  >
+                    {borderCountryName}
+                  </Link>
                 ))}
               </div>
             </div>
